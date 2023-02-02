3b4315d45099ff9cdc81f2461dd9c15e44d0eb53
Alexandre Scelles <alexandrescelles@yahoo.fr>
2023-02-02 16:43:38 +0100

    About page done

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -11,8 +11,8 @@ const IndexRoutes = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route exact path="/home" element={<Home />} />
-                {/* <Route exact path="/about" element={<About />} />
-                <Route exact path="/housing/:id" element={<Housing />} />
+                <Route exact path="/about" element={<About />} />
+                {/*<Route exact path="/housing/:id" element={<Housing />} />
                 <Route path="*" element={<NoMatch />} /> */}
             </Routes>
         </BrowserRouter>
